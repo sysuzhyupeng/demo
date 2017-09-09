0ca6d1446c9c2c01c98bc8ef12058804c675a879
sysuzhyupeng <673341297@qq.com>
2017-09-09 19:19:43 +0800

    change Card.js

---
+++ source/Card.js
@@ -0,0 +1,13 @@
+import React, { Component }from 'react'
+import CheckList from './CheckList'
+
+class Cards extends Component {
+	render(){
+		return (
+			<div className="card">
+				<div className="card_title">{this.props.title}</div>
+				<div className=
+			</div>
+		)
+	}
+}
